ad648da097636917657d14920273c4e541bc870f
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-07 17:33:46 -0700

    add setup

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/sh
+
+VERSION=0.12.19
+
+set -eax
+
+# This is all the stuff obsidian needs to run
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+    xvfb \
+    libgtk-3-0 \
+    libnotify4 \
+    libnss3 \
+    libxss1 \
+    libxtst6 \
+    xdg-utils \
+    libatspi2.0-0 \
+    libuuid1 \
+    libsecret-1-0 \
+    libasound2
+
+rm -rf ./.build/app/
+mkdir -p ./.build/app/
+cd ./.build/app/ || true
+
+curl -OL "https://github.com/obsidianmd/obsidian-releases/releases/download/v${VERSION}/Obsidian-${VERSION}.AppImage"
+mv "Obsidian-${VERSION}.AppImage" "obsidian.AppImage"
+chmod +x ./obsidian.AppImage
+
+./obsidian.AppImage --appimage-extract
+rm -rf ../obsidian/
+mkdir -p ../obsidian/
+mv ./squashfs-root/* ../obsidian/
+chmod 4755 ../obsidian/chrome-sandbox
+
+
+export DISPLAY=":99"
+xvfb-run yarn test
