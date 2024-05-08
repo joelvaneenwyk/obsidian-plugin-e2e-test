555fc06b1c9d6e997b523e4b2eb2e06613ddf577
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-05-07 17:58:49 -0700

    add args

--- setup.sh
+++ setup.sh
@@ -1,28 +1,31 @@
 #!/usr/bin/sh
 
-VERSION=0.12.19
-
 set -eax
 
-# This is all the stuff obsidian needs to run
-sudo apt-get update && sudo apt-get install -y --no-install-recommends \
-    xvfb \
-    libgtk-3-0 \
-    libnotify4 \
-    libnss3 \
-    libxss1 \
-    libxtst6 \
-    xdg-utils \
-    libatspi2.0-0 \
-    libuuid1 \
-    libsecret-1-0 \
-    libasound2
+# Install everything Obsidian needs to run
+if command -v sudo && command -v apt-get; then
+    sudo apt-get update
+    sudo apt-get install -y --no-install-recommends \
+        curl \
+        xvfb \
+        libgtk-3-0 \
+        libnotify4 \
+        libnss3 \
+        libxss1 \
+        libxtst6 \
+        xdg-utils \
+        libatspi2.0-0 \
+        libuuid1 \
+        libsecret-1-0 \
+        libasound2
+fi
 
 (
     rm -rf ./.build/app/
     mkdir -p ./.build/app/
     cd ./.build/app/ || true
 
+    VERSION="${VERSION:-0.12.19}"
     curl -OL "https://github.com/obsidianmd/obsidian-releases/releases/download/v${VERSION}/Obsidian-${VERSION}.AppImage"
     mv "Obsidian-${VERSION}.AppImage" "obsidian.AppImage"
     chmod +x ./obsidian.AppImage
@@ -34,10 +37,12 @@ sudo apt-get update && sudo apt-get install -y --no-install-recommends \
     chmod 4755 ../obsidian/chrome-sandbox
 )
 
-yarn install
-yarn build
+if [ "${1:-}" = "test" ]; then
+    yarn install
+    yarn build
 
-export DISPLAY=":99"
+    export DISPLAY=":99"
+    export OBSIDIAN_PATH=./.build/obsidian/obsidian
 
-export OBSIDIAN_PATH=./.build/obsidian/obsidian
-xvfb-run yarn test
+    xvfb-run yarn test
+fi
